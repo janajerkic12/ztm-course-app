0e805b444b5c4f64adf782ca6bf0fd7b550fcf72
Jana Jerkic <jana.jerkic@commercecare.de>
2022-04-01 11:53:03 +0200

    Adding functionallity to search box

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
       
     }
   }
@@ -22,11 +23,24 @@ class App extends Component {
   }
 
   render() {
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    })
+
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='serach monster...' />
+        <input className='search-box' type='search' placeholder='serach monster...' onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase();
+
+          
+
+          this.setState(() => {
+            return {searchField};
+          })
+        }}/>
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
